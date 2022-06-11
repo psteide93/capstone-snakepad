ca9bb3157e083c3cb44da4b91ef7a1778f0012be
Parnell <psteide@ford.com>
2022-06-11 19:12:24 -0400

    Add landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { AuthGuard } from '@auth0/auth0-angular';
+import { SnakeListingComponent } from './components/snake-listing/snake-listing.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' },
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'snakes',
+    component: SnakeListingComponent,
+    canActivate: [AuthGuard],
+  },
 ];
 
 @NgModule({
