c0ef73d1f47cb85090ffc934e6f067bf0f6e6979
Parnell <psteide@ford.com>
2022-06-13 12:48:07 -0400

    Add logic to collapse record form

--- src/app/components/record-add-form/record-add-form.component.ts
+++ src/app/components/record-add-form/record-add-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UiService } from 'src/assets/ultilities/services/ui.service';
 
 @Component({
   selector: 'app-record-add-form',
@@ -7,8 +9,14 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class RecordAddFormComponent implements OnInit {
   @Input() record: string = '';
+  showAddRecord = false;
+  subscription: Subscription;
 
-  constructor() {}
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddRecord = value));
+  }
 
   ngOnInit(): void {}
 }
